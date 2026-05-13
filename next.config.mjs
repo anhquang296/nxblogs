de7297e80c2be2999149cd97e0577bbf4e27d271
Aiden <quang.tran290699@gmail.com>
2026-05-13 20:21:16 +0700

    Enhance code styling with custom Nextra code block styles and update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,15 @@ import nextra from 'nextra'
 
 const withNextra = nextra({
   defaultShowCopyCode: true,
-  readingTime: true
+  readingTime: true,
+  mdxOptions: {
+    rehypePrettyCodeOptions: {
+      theme: {
+        light: 'one-dark-pro',
+        dark: 'one-dark-pro',
+      },
+    },
+  },
 })
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
